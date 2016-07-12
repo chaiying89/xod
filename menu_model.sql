1825bf9fe24c4a3fe45e871ba7a504a24febffef
chaiying89 <364070589@qq.com>
2016-07-12 16:28:21 +0800

    初始化sql更新

--- menu_model.sql
+++ menu_model.sql
@@ -2,7 +2,7 @@ INSERT INTO `menu_model` VALUES (1, '2016-6-28 16:45:38', 0, 1, '1', '首页', '
 INSERT INTO `menu_model` VALUES (2, '2016-6-28 16:45:49', 0, 1, '1', '关于我们', '2016-6-28 16:46:01', '/about/13', 1, NULL, 1, 2);
 INSERT INTO `menu_model` VALUES (3, '2016-6-28 16:45:40', 0, 1, '1', '资讯中心', '2016-6-28 16:45:52', '/info/9', 1, NULL, 1, 3);
 INSERT INTO `menu_model` VALUES (4, '2016-6-28 16:45:41', 0, 1, '1', '投资加盟', '2016-6-28 16:45:52', '/touzi/18', 1, NULL, 1, 4);
-INSERT INTO `menu_model` VALUES (5, '2016-6-28 16:45:42', 0, 1, '1', '经营范围', '2016-6-28 16:45:52', '/fanwei', 1, NULL, 1, 5);
+INSERT INTO `menu_model` VALUES (5, '2016-6-28 16:45:42', 0, 1, '1', '经营范围', '2016-6-28 16:45:52', '/scope', 1, NULL, 1, 5);
 INSERT INTO `menu_model` VALUES (6, '2016-6-28 16:45:43', 0, 1, '1', '养生专区', '2016-6-28 16:45:52', '/yangsheng', 1, NULL, 1, 6);
 INSERT INTO `menu_model` VALUES (7, '2016-6-28 16:45:44', 0, 1, '1', '联系我们', '2016-6-28 16:45:52', '/contact', 1, NULL, 1, 7);
 INSERT INTO `menu_model` VALUES (8, '2016-6-28 16:45:45', 0, 1, '1', '招聘专栏', '2016-6-28 16:45:52', '/zhaopin', 1, NULL, 1, 8);
@@ -21,4 +21,10 @@ INSERT INTO `menu_model` VALUES (20, '2016-7-4 16:29:41', 1, 0, '2', '管理一�
 INSERT INTO `menu_model` VALUES (21, '2016-7-4 16:29:41', 1, 0, '2', '管理团队', '2016-7-4 16:29:52', '/touzi/21', 1, 4, 1, 4);
 INSERT INTO `menu_model` VALUES (22, '2016-7-4 16:29:41', 1, 1, '2', '门店展示', '2016-7-4 16:29:52', '/touzi/stores/22', 1, 4, 1, 5);
 INSERT INTO `menu_model` VALUES (23, '2016-7-4 16:29:41', 1, 0, '2', '如何合作', '2016-7-4 16:29:52', '/touzi/23', 1, 4, 1, 6);
-INSERT INTO `menu_model` VALUES (24, '2016-7-4 16:29:41', 1, 1, '2', '疑难问答', '2016-7-4 16:29:52', '/touzi/questions/24', 1, 4, 1, 7);
+INSERT INTO `menu_model` VALUES (24, '2016-7-4 16:29:41', 1, 0, '2', '疑难问答', '2016-7-4 16:29:52', '/touzi/24', 1, 4, 1, 7);
+INSERT INTO `menu_model` VALUES (25, '2016-7-4 16:29:41', 1, 0, '3', '门店组织构架', '2016-7-4 16:29:52', '', 1, 22, 1, 1);
+INSERT INTO `menu_model` VALUES (26, '2016-7-4 16:29:41', 1, 0, '3', '\r\n\r\n部门管理', '2016-7-4 16:29:52', '', 1, 22, 1, 2);
+INSERT INTO `menu_model` VALUES (27, '2016-7-4 16:29:41', 1, 1, '3', '\r\n\r\n门店形象展示', '2016-7-4 16:29:52', '', 1, 22, 1, 3);
+INSERT INTO `menu_model` VALUES (28, '2016-7-4 16:29:41', 1, 0, '2', '浴足连锁', '2016-7-4 16:29:52', '/scope/28', 1, 5, 1, 1);
+INSERT INTO `menu_model` VALUES (29, '2016-7-4 16:29:41', 1, 0, '2', '酒店管理', '2016-7-4 16:29:52', '/scope/29', 1, 5, 1, 2);
+INSERT INTO `menu_model` VALUES (30, '2016-7-4 16:29:41', 1, 0, '2', '企业人才培训', '2016-7-4 16:29:52', '/scope/30', 1, 5, 1, 3);
